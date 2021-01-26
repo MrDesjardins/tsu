edf1a591847d173267b26e313c17fbdfd04293bf
Patrick Desjardins <pdesjardins@netflix.com>
2021-01-25 21:46:14 -0800

    Update Prettier to a shorter length that help for video recording

--- TSU027/tsu027_1.ts
+++ TSU027/tsu027_1.ts
@@ -1,9 +1,12 @@
 namespace TSU027_1 {
-  let hugeNumber = Number.MAX_SAFE_INTEGER + 1;
+  let hugeNumber =
+    Number.MAX_SAFE_INTEGER + 1;
   console.log(hugeNumber);
   console.log(hugeNumber + 1);
 
-  let hugeBigInt = BigInt(Number.MAX_SAFE_INTEGER) + 1n;
+  let hugeBigInt =
+    BigInt(Number.MAX_SAFE_INTEGER) +
+    1n;
   console.log(hugeBigInt);
   console.log(hugeBigInt + 1n);
 }
